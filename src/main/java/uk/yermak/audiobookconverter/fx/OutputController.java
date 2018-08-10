96ace557f4b4b3a78ec2c65663bd557f60c2388b
Yaroslav Yermak <yermak@gmail.com>
2018-08-10 11:55:19 +0100

    Output quality support

--- src/main/java/uk/yermak/audiobookconverter/fx/OutputController.java
+++ src/main/java/uk/yermak/audiobookconverter/fx/OutputController.java
@@ -1,21 +1,45 @@
 package uk.yermak.audiobookconverter.fx;
 
 import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.CheckBox;
+import javafx.scene.control.RadioButton;
+import javafx.scene.control.Slider;
 import javafx.scene.control.Spinner;
 
 /**
  * Created by yermak on 08/09/2018.
  */
 public class OutputController {
-    public Spinner bitRate;
-    public Spinner frequency;
-    public Spinner channels;
+    @FXML
+    private Spinner bitRate;
+    @FXML
+    private Spinner frequency;
+    @FXML
+    private Spinner channels;
+    @FXML
+    private Slider quality;
+    @FXML
+    private RadioButton cbr;
+    @FXML
+    private RadioButton vbr;
+    @FXML
+    private CheckBox auto;
+    @FXML
+    private Spinner parts;
 
     public void cbr(ActionEvent actionEvent) {
 
     }
 
     public void vbr(ActionEvent actionEvent) {
+
+    }
+
+    @FXML
+    private void initialize() {
+        auto.setSelected(true);
+        parts.getValueFactory().setValue(2);
 
     }
 }

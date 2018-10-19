a578a3c22b2ea3bb790b47ad4f4394d8f8e963a7
Yaroslav Yermak <yermak@gmail.com>
2018-10-19 21:18:26 +0100

    Version update

--- src/main/java/uk/yermak/audiobookconverter/Version.java
+++ src/main/java/uk/yermak/audiobookconverter/Version.java
@@ -2,12 +2,14 @@ package uk.yermak.audiobookconverter;
 
 public class Version {
     public static final int MAJOR = 2;
-    public static final int MINOR = 0;
+    public static final int MINOR = 8;
+    public static final int BUILD = 0;
 
     public Version() {
     }
 
     public static String getVersionString() {
-        return MAJOR + "." + MINOR;
+        //TODO add load version from the build number.
+        return MAJOR + "." + MINOR + (BUILD != 0 ? "." + BUILD : "");
     }
 }
--- src/main/java/uk/yermak/audiobookconverter/fx/ConverterApplication.java
+++ src/main/java/uk/yermak/audiobookconverter/fx/ConverterApplication.java
@@ -9,6 +9,7 @@ import javafx.scene.Scene;
 import javafx.stage.Screen;
 import javafx.stage.Stage;
 import uk.yermak.audiobookconverter.ConversionContext;
+import uk.yermak.audiobookconverter.Version;
 
 import java.io.IOException;
 import java.net.URL;
@@ -33,7 +34,7 @@ public class ConverterApplication extends Application {
 
         Scene scene = new Scene(root);
 
-        stage.setTitle("AudioBookConverter V2");
+        stage.setTitle("AudioBookConverter " + Version.getVersionString());
         stage.setScene(scene);
         Screen primary = Screen.getPrimary();
         stage.setMinHeight(primary.getVisualBounds().getHeight() * 0.5);

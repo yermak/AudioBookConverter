fae4d1cbc708e0cccdfe18b8be39b33e8acfd7d1
yermak <yermak@gmail.com>
2019-01-02 16:29:49 +0000

    Clean-up code

--- src/main/java/uk/yermak/audiobookconverter/ConversionMode.java
+++ src/main/java/uk/yermak/audiobookconverter/ConversionMode.java
@@ -8,37 +8,20 @@ import java.util.Map;
 public enum ConversionMode {
     SINGLE {
         @Override
-        public boolean supportTags() {
-            return true;
-        }
-
-        @Override
         public JoiningConversionStrategy createConvertionStrategy(Conversion conversion, Map<String, ProgressCallback> progressCallbacks) {
             return new JoiningConversionStrategy(conversion, progressCallbacks);
         }
     }, BATCH {
-        @Override
-        public boolean supportTags() {
-            return false;
-        }
-
         @Override
         public ConversionStrategy createConvertionStrategy(Conversion conversion, Map<String, ProgressCallback> progressCallbacks) {
             return new BatchConversionStrategy(conversion, progressCallbacks);
         }
     }, PARALLEL {
         @Override
-        public boolean supportTags() {
-            return true;
-        }
-
-        @Override
         public ConversionStrategy createConvertionStrategy(Conversion conversion, Map<String, ProgressCallback> progressCallbacks) {
             return new ParallelConversionStrategy(conversion, progressCallbacks);
         }
     };
-
-    public abstract boolean supportTags();
 
     public abstract ConversionStrategy createConvertionStrategy(Conversion conversion, Map<String, ProgressCallback> progressCallbacks);
 }

1c3c0fa2cf902552c3a90d1a7393127ceadc9a46
yermak <yermak@gmail.com>
2019-08-02 10:44:48 +0100

    fix for issue #24.

--- src/main/scala/uk/yermak/audiobookconverter/Version.scala
+++ src/main/scala/uk/yermak/audiobookconverter/Version.scala
@@ -3,7 +3,7 @@ package uk.yermak.audiobookconverter
 object Version {
   val MAJOR = 3
   val MINOR = 2
-  val BUILD = 0
+  val BUILD = 1
 
   def getVersionString: String = { //TODO add load version from the build number.
     "AudioBookConverter " + MAJOR + "." + MINOR + (if (BUILD != 0) "." + BUILD

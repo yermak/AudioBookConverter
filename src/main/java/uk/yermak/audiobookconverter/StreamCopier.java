7cc996db6de09487aee325024872fff8f128d311
yermak <yermak@gmail.com>
2020-01-02 13:13:10 +0000

    clean-up

--- src/main/java/uk/yermak/audiobookconverter/StreamCopier.java
+++ src/main/java/uk/yermak/audiobookconverter/StreamCopier.java
@@ -4,6 +4,8 @@ import org.apache.commons.io.IOUtils;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
+import java.io.Closeable;
+import java.io.IOException;
 import java.io.InputStream;
 import java.io.OutputStream;
 import java.lang.invoke.MethodHandles;
@@ -36,13 +38,23 @@ public class StreamCopier implements Callable<Long> {
         for (int n; -1 != (n = in.read(buffer)); count += (long) n) {
             out.write(buffer, 0, n);
         }
-        IOUtils.closeQuietly(in);
-        IOUtils.closeQuietly(out);
+        closeQuietly(in);
+        closeQuietly(out);
         return count;
     }
 
 
     public static Future<Long> copy(InputStream in, OutputStream out) {
         return executorService.submit(new StreamCopier(in, out));
+    }
+
+    public static void closeQuietly(final Closeable closeable) {
+        try {
+            if (closeable != null) {
+                closeable.close();
+            }
+        } catch (final IOException ioe) {
+            // ignore
+        }
     }
 }

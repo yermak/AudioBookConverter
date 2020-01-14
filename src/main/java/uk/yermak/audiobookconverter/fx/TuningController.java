740f1adc6ef670701eb333f4efa4beb8df34ca66
yermak <yermak@gmail.com>
2020-01-14 08:56:39 +0000

    clean-up

--- src/main/java/uk/yermak/audiobookconverter/fx/TuningController.java
+++ src/main/java/uk/yermak/audiobookconverter/fx/TuningController.java
@@ -4,8 +4,6 @@ import javafx.fxml.FXML;
 import javafx.scene.control.Slider;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import uk.yermak.audiobookconverter.Conversion;
-import uk.yermak.audiobookconverter.ConversionSubscriber;
 import uk.yermak.audiobookconverter.OutputParameters;
 
 import java.lang.invoke.MethodHandles;

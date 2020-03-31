713413a00dc42ce50b6a52375b81e5652df60e71
yermak <yermak@gmail.com>
2020-03-31 21:49:33 +0100

    version bump

--- src/main/java/uk/yermak/audiobookconverter/Version.java
+++ src/main/java/uk/yermak/audiobookconverter/Version.java
@@ -2,7 +2,7 @@ package uk.yermak.audiobookconverter;
 
 public class Version {
     private final static int MAJOR = 4;
-    private final static int MINOR = 4;
+    private final static int MINOR = 5;
     private final static int BUILD = 0;
 
     public static String getVersionString() {

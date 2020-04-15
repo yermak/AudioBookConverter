f5dfc6706e6bb121a862910df753725dd74562fa
yermak <yermak@gmail.com>
2020-04-15 07:59:42 +0100

    UI Rework, clean-up

--- src/main/java/uk/yermak/audiobookconverter/Subscriber.java
+++
@@ -1,7 +0,0 @@
-package uk.yermak.audiobookconverter;
-
-import uk.yermak.audiobookconverter.fx.ConversionProgress;
-
-public interface Subscriber {
-    void addConversionProgress(ConversionProgress conversionProgress);
-}

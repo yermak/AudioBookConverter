954fafda0d2ed7abda93b9ab31085845c91687dc
yermak <yermak@gmail.com>
2020-04-17 22:48:53 +0100

    fixed deb name

--- src/main/deploy/package/linux/package.sh
+++ src/main/deploy/package/linux/package.sh
@@ -4,4 +4,4 @@ export APP_VERSION=$3
 rm -rf target/fx-jre
 $JAVA_HOME/bin/jlink --module-path $JAVA_HOME/jmods:$JAVAFX_MODS --add-modules java.base,java.sql,javafx.controls,javafx.fxml,javafx.media,javafx.base,javafx.swing,javafx.graphics --output target/fx-jre
 $JAVA_HOME/bin/jpackage --app-version $APP_VERSION  --license-file README.md --icon AudioBookConverter.png -t deb --name AudioBookConverter --vendor Recoupler --input target/package/audiobookconverter-$APP_VERSION/audiobookconverter-$APP_VERSION/app --main-jar lib/audiobookconverter-$APP_VERSION.jar --runtime-image target/fx-jre
-mv AudioBookConverter-$APP_VERSION.deb target/
+mv AudioBookConverter-$APP_VERSION-1_amd64.deb target/

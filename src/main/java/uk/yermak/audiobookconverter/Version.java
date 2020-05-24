7bc1a6a8e7d5726b2e9bf270070071ef446db392
yermak <yermak@gmail.com>
2020-05-24 21:03:57 +0100

    version 5.2.0 release

--- src/main/java/uk/yermak/audiobookconverter/Version.java
+++ src/main/java/uk/yermak/audiobookconverter/Version.java
@@ -2,7 +2,7 @@ package uk.yermak.audiobookconverter;
 
 public class Version {
     private final static int MAJOR = 5;
-    private final static int MINOR = 1;
+    private final static int MINOR = 2;
     private final static int BUILD = 0;
 
     public static String getVersionString() {

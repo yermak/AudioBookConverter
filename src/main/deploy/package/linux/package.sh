ea0f5e0a1185483cb7e8d03b74376f98ba388ff2
yermak <yermak@gmail.com>
2020-05-24 18:28:17 +0100

    packaging fix

--- src/main/deploy/package/linux/package.sh
+++ src/main/deploy/package/linux/package.sh
@@ -7,7 +7,7 @@ $JAVA_HOME/bin/jlink --module-path $JAVA_HOME/jmods:$JAVAFX_MODS \
 
 $JAVA_HOME/bin/jpackage --app-version %APP_VERSION%  -t app-image --name AudioBookConverter --vendor Recoupler \
 --input target/package/audiobookconverter-%APP_VERSION%/audiobookconverter-%APP_VERSION%/app \
---main-jar lib/audiobookconverter-%APP_VERSION%.jar --runtime-image target/fx-jre --dest target/image
+--main-jar lib/audiobookconverter-%APP_VERSION%.jar --runtime-image target/fx-jre --dest target/image --java-options '--enable-preview'
 
 
 $JAVA_HOME/bin/jpackage --app-version $APP_VERSION  --license-file README.md --icon AudioBookConverter.png \
@@ -15,7 +15,7 @@ $JAVA_HOME/bin/jpackage --app-version $APP_VERSION  --license-file README.md --i
 --linux-menu-group AudioBookConverter --linux-shortcut \
 --linux-package-deps ffmpeg mp4v2-utils \
 --input target/package/audiobookconverter-$APP_VERSION/audiobookconverter-$APP_VERSION/app \
---main-jar lib/audiobookconverter-$APP_VERSION.jar --runtime-image target/fx-jre
+--main-jar lib/audiobookconverter-$APP_VERSION.jar --runtime-image target/fx-jre --java-options '--enable-preview'
 mv audiobookconverter-$APP_VERSION-1_amd64.deb target/
 
 $JAVA_HOME/bin/jpackage --app-version $APP_VERSION  --license-file README.md --icon AudioBookConverter.png \
@@ -23,4 +23,5 @@ $JAVA_HOME/bin/jpackage --app-version $APP_VERSION  --license-file README.md --i
 --linux-menu-group AudioBookConverter --linux-shortcut \
 --linux-package-deps ffmpeg mp4v2-utils \
 --input target/package/audiobookconverter-$APP_VERSION/audiobookconverter-$APP_VERSION/app \
---main-jar lib/audiobookconverter-$APP_VERSION.jar --runtime-image target/fx-jre
+--main-jar lib/audiobookconverter-$APP_VERSION.jar --runtime-image target/fx-jre --java-options '--enable-preview'
+mv audiobookconverter-$APP_VERSION-1_amd64.rpm target/

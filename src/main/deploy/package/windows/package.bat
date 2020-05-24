d6176ddd06a8cfa907a8847d063aaedb07238730
yermak <yermak@gmail.com>
2020-05-24 18:17:41 +0100

    packaging

--- src/main/deploy/package/windows/package.bat
+++ src/main/deploy/package/windows/package.bat
@@ -7,14 +7,14 @@ rmdir /s /q target\fx-jre
 
 %JAVA_HOME%\bin\jpackage.exe --app-version %APP_VERSION%  --icon AudioBookConverter.ico -t app-image --name AudioBookConverter --vendor Recoupler --input target/package/audiobookconverter-%APP_VERSION%/audiobookconverter-%APP_VERSION%/app --main-jar lib/audiobookconverter-%APP_VERSION%.jar --runtime-image target/fx-jre --dest target/image
 cd target\image\
-7z a -t7z -mx9 -mmt8 -sfx7z.sfx AudioBookConverter-SFX-%APP_VERSION%.exe AudioBookConverter
-move AudioBookConverter-SFX-%APP_VERSION%.exe ..\
+7z a -t7z -mx9 -mmt8 -sfx7z.sfx AudioBookConverter-Portable-%APP_VERSION%.exe AudioBookConverter
+move AudioBookConverter-Portable-%APP_VERSION%.exe ..\
 cd ..\..
 
 %JAVA_HOME%\bin\jpackage.exe --app-version %APP_VERSION%  --license-file README.md --icon AudioBookConverter.ico -t msi --win-dir-chooser --win-shortcut --win-menu --win-menu-group AudioBookConverter --name AudioBookConverter --vendor Recoupler --input target/package/audiobookconverter-%APP_VERSION%/audiobookconverter-%APP_VERSION%/app --main-jar lib/audiobookconverter-%APP_VERSION%.jar --runtime-image target/fx-jre
 move AudioBookConverter-%APP_VERSION%.msi target/AudioBookConverter-All-Users-%APP_VERSION%.msi
 
-%JAVA_HOME%\bin\jpackage.exe --app-version %APP_VERSION%  --license-file README.md --icon AudioBookConverter.ico --win-per-user-install -t msi --win-dir-chooser --win-shortcut --win-menu --win-menu-group AudioBookConverter --name AudioBookConverter --vendor Recoupler --input target/package/audiobookconverter-%APP_VERSION%/audiobookconverter-%APP_VERSION%/app --main-jar lib/audiobookconverter-%APP_VERSION%.jar --runtime-image target/fx-jre
+%JAVA_HOME%\bin\jpackage.exe --app-version %APP_VERSION%  --license-file README.md --icon AudioBookConverter.ico --win-per-user-install -t msi --win-shortcut --win-menu --win-menu-group AudioBookConverter --name AudioBookConverter --vendor Recoupler --input target/package/audiobookconverter-%APP_VERSION%/audiobookconverter-%APP_VERSION%/app --main-jar lib/audiobookconverter-%APP_VERSION%.jar --runtime-image target/fx-jre
 move AudioBookConverter-%APP_VERSION%.msi target/AudioBookConverter-Single-User-%APP_VERSION%.msi
 
 

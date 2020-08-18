ab75d458fed2d8a1f2a85debbe89b2aa6fe1bb6d
yermak <yermak@gmail.com>
2020-08-18 08:33:01 +0100

    Update version, rollbacked openJFX to 14

--- src/main/java/uk/yermak/audiobookconverter/Version.java
+++ src/main/java/uk/yermak/audiobookconverter/Version.java
@@ -3,7 +3,7 @@ package uk.yermak.audiobookconverter;
 public class Version {
     private final static int MAJOR = 5;
     private final static int MINOR = 3;
-    private final static int BUILD = 0;
+    private final static int BUILD = 1;
 
     public static String getVersionString() {
         return "AudioBookConverter-" + MAJOR + "." + MINOR + "." + BUILD;

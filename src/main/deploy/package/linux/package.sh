1e8312a22136ebeec9d4700a1fa7942900d760ca
yermak <yermak@gmail.com>
2020-11-16 10:09:27 +0000

    issue #121, linux release build

--- src/main/deploy/package/linux/package.sh
+++ src/main/deploy/package/linux/package.sh
@@ -5,9 +5,9 @@ rm -rf target/fx-jre
 $JAVA_HOME/bin/jlink --module-path $JAVA_HOME/jmods:$JAVAFX_MODS \
 --add-modules java.base,java.sql,javafx.controls,javafx.fxml,javafx.media,javafx.base,javafx.swing,javafx.graphics --output target/fx-jre
 
-$JAVA_HOME/bin/jpackage --app-version %APP_VERSION%  -t app-image --name AudioBookConverter --vendor Recoupler \
---input target/package/audiobookconverter-%APP_VERSION%-linux-installer/audiobookconverter-%APP_VERSION%/app \
---main-jar lib/audiobookconverter-%APP_VERSION%.jar --runtime-image target/fx-jre --dest target/image --java-options '--enable-preview'
+$JAVA_HOME/bin/jpackage --app-version $APP_VERSION  -t app-image --name AudioBookConverter --vendor Recoupler \
+--input target/package/audiobookconverter-$APP_VERSION-linux-installer/audiobookconverter-$APP_VERSION/app \
+--main-jar lib/audiobookconverter-$APP_VERSION.jar --runtime-image target/fx-jre --dest target/image --java-options '--enable-preview'
 
 
 $JAVA_HOME/bin/jpackage --app-version $APP_VERSION  --license-file README.md --icon AudioBookConverter.png \

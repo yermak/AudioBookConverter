bb34ad67dd64c3aef76139fd14fa296e35494303
yermak <yermak@gmail.com>
2020-11-16 23:01:47 +0000

    issue #122, mac release build

--- src/main/deploy/package/mac/package.sh
+++ src/main/deploy/package/mac/package.sh
@@ -6,9 +6,9 @@ $JAVA_HOME/bin/jlink --module-path $JAVA_HOME/jmods:$JAVAFX_MODS \
 --add-modules java.base,java.sql,javafx.controls,javafx.fxml,javafx.media,javafx.base,javafx.swing,javafx.graphics --output target/fx-jre
 
 
-$JAVA_HOME/bin/jpackage --app-version $APP_VERSION  --license-file README.md --icon AudioBookConverter.icns \
+$JAVA_HOME/bin/jpackage --app-version $APP_VERSION  --license-file LICENSE --icon AudioBookConverter.icns \
 --type dmg \
 --input target/package/audiobookconverter-$APP_VERSION-mac-installer/audiobookconverter-$APP_VERSION/app \
 --main-jar lib/audiobookconverter-$APP_VERSION.jar --runtime-image target/fx-jre --java-options '--enable-preview'
 mkdir target/release
-mv *.dmg target/release
+mv -f *.dmg target/release

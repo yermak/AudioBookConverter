448b7f4e9ebe8f76b619d950a44dfaede23af60c
yermak <yermak@gmail.com>
2020-11-17 07:49:06 +0000

    issue #120, win release build

--- src/main/deploy/package/windows/package.bat
+++ src/main/deploy/package/windows/package.bat
@@ -11,13 +11,16 @@ rmdir /s /q target\image\AudioBookConverter
 
 cd target\image\
 7z.exe a -t7z -mx9 -mmt8 -sfx7z.sfx AudioBookConverter-Portable-%APP_VERSION%.exe AudioBookConverter
+dir
 move AudioBookConverter-Portable-%APP_VERSION%.exe ..\release\
 cd ..\..
 
 %JAVA_HOME%\bin\jpackage.exe --app-version %APP_VERSION%  --license-file README.md --icon AudioBookConverter.ico -t msi --win-dir-chooser --win-shortcut --win-menu --win-menu-group AudioBookConverter --name AudioBookConverter --vendor Recoupler --input target/package/audiobookconverter-%APP_VERSION%-windows-installer/audiobookconverter-%APP_VERSION%/app --main-jar lib/audiobookconverter-%APP_VERSION%.jar --runtime-image target/fx-jre --java-options '--enable-preview'
+dir
 move AudioBookConverter-%APP_VERSION%.msi target/release/AudioBookConverter-All-Users-%APP_VERSION%.msi
 
 %JAVA_HOME%\bin\jpackage.exe --app-version %APP_VERSION%  --license-file README.md --icon AudioBookConverter.ico --win-per-user-install -t msi --win-shortcut --win-menu --win-menu-group AudioBookConverter --name AudioBookConverter --vendor Recoupler --input target/package/audiobookconverter-%APP_VERSION%-windows-installer/audiobookconverter-%APP_VERSION%/app --main-jar lib/audiobookconverter-%APP_VERSION%.jar --runtime-image target/fx-jre --java-options '--enable-preview'
+dir
 move AudioBookConverter-%APP_VERSION%.msi target/release/AudioBookConverter-Single-User-%APP_VERSION%.msi
 
 

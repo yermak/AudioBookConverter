637109624be887fb4e71b3b0f29cf2e846ceb6c4
yermak <yermak@gmail.com>
2021-05-15 16:24:48 +0100

    files dialogs

--- src/main/java/uk/yermak/audiobookconverter/fx/FilesController.java
+++ src/main/java/uk/yermak/audiobookconverter/fx/FilesController.java
@@ -14,9 +14,6 @@ import javafx.fxml.FXML;
 import javafx.geometry.Side;
 import javafx.scene.control.*;
 import javafx.scene.input.TransferMode;
-import javafx.stage.DirectoryChooser;
-import javafx.stage.FileChooser;
-import javafx.stage.Window;
 import org.apache.commons.io.FileUtils;
 import org.apache.commons.io.FilenameUtils;
 import org.apache.commons.io.IOCase;
@@ -82,14 +79,6 @@ public class FilesController {
     private Button startButton;
 
 
-    private static final String M4B = "m4b";
-    private static final String M4A = "m4a";
-    public static final String MP3 = "mp3";
-    public static final String WMA = "wma";
-    public static final String FLAC = "flac";
-    public static final String AAC = "aac";
-    public static final String OGG = "ogg";
-    private final static String[] FILE_EXTENSIONS = {MP3, M4A, M4B, WMA, FLAC, OGG, AAC};
 
     private final ContextMenu contextMenu = new ContextMenu();
 
@@ -151,9 +140,9 @@ public class FilesController {
 
     private void initFileOpenMenu() {
         MenuItem item1 = new MenuItem("Files");
-        item1.setOnAction(e -> selectFilesDialog());
+        item1.setOnAction(e -> selectFiles());
         MenuItem item2 = new MenuItem("Folder");
-        item2.setOnAction(e -> selectFolderDialog());
+        item2.setOnAction(e -> selectFolder());
         contextMenu.getItems().addAll(item1, item2);
     }
 
@@ -168,13 +157,17 @@ public class FilesController {
         });
 
         control.setOnDragDropped(event -> {
-            processFiles(event.getDragboard().getFiles());
-            event.setDropCompleted(true);
-            event.consume();
-            if (!chaptersMode.get()) {
-                if (!filesChapters.getTabs().contains(filesTab)) {
-                    filesChapters.getTabs().add(filesTab);
-                    filesChapters.getSelectionModel().select(filesTab);
+            List<File> files = event.getDragboard().getFiles();
+            if (files!=null && !files.isEmpty()) {
+                List<String> fileNames = DialogHelper.collectFiles(files);
+                processFiles(fileNames);
+                event.setDropCompleted(true);
+                event.consume();
+                if (!chaptersMode.get()) {
+                    if (!filesChapters.getTabs().contains(filesTab)) {
+                        filesChapters.getTabs().add(filesTab);
+                        filesChapters.getSelectionModel().select(filesTab);
+                    }
                 }
             }
         });
@@ -187,21 +180,10 @@ public class FilesController {
         contextMenu.show(node, Side.RIGHT, 0, 0);
     }
 
-    public void selectFolderDialog() {
-        Window window = ConverterApplication.getEnv().getWindow();
-        DirectoryChooser directoryChooser = new DirectoryChooser();
-        String sourceFolder = AppProperties.getProperty("source.folder");
-        directoryChooser.setInitialDirectory(Utils.getInitialDirecotory(sourceFolder));
-
-        StringJoiner filetypes = new StringJoiner("/");
-
-        Arrays.stream(FILE_EXTENSIONS).map(String::toUpperCase).forEach(filetypes::add);
-
-        directoryChooser.setTitle("Select folder with " + filetypes.toString() + " files for conversion");
-        File selectedDirectory = directoryChooser.showDialog(window);
-        if (selectedDirectory != null) {
-            processFiles(Collections.singleton(selectedDirectory));
-            AppProperties.setProperty("source.folder", selectedDirectory.getAbsolutePath());
+    public void selectFolder() {
+        List<String> fileNames = DialogHelper.selectFolderDialog();
+        if (fileNames != null) {
+            processFiles(fileNames);
             if (!chaptersMode.get()) {
                 if (!filesChapters.getTabs().contains(filesTab)) {
                     filesChapters.getTabs().add(filesTab);
@@ -212,9 +194,7 @@ public class FilesController {
     }
 
 
-    private void processFiles(Collection<File> files) {
-        List<String> fileNames = collectFiles(files);
-
+    private void processFiles(List<String> fileNames) {
         List<MediaInfo> addedMedia = createMediaLoader(fileNames).loadMediaInfo();
         if (chaptersMode.get()) {
             Book book = ConverterApplication.getContext().next().getBook();
@@ -225,60 +205,15 @@ public class FilesController {
         }
     }
 
-    private static String[] toSuffixes(String prefix, final String[] extensions) {
-        final String[] suffixes = new String[extensions.length];
-        for (int i = 0; i < extensions.length; i++) {
-            suffixes[i] = prefix + extensions[i];
-        }
-        return suffixes;
-    }
-
-    private List<String> collectFiles(Collection<File> files) {
-        List<String> fileNames = new ArrayList<>();
-        ImmutableSet<String> extensions = ImmutableSet.copyOf(FILE_EXTENSIONS);
-
-        for (File file : files) {
-            if (file.isDirectory()) {
-                SuffixFileFilter suffixFileFilter = new SuffixFileFilter(toSuffixes(".", FILE_EXTENSIONS), IOCase.INSENSITIVE);
-                Collection<File> nestedFiles = FileUtils.listFiles(file, suffixFileFilter, TrueFileFilter.INSTANCE);
-                nestedFiles.stream().map(File::getPath).forEach(fileNames::add);
-            } else {
-                boolean allowedFileExtension = extensions.contains(FilenameUtils.getExtension(file.getName()).toLowerCase());
-                if (allowedFileExtension) {
-                    fileNames.add(file.getPath());
-                }
-            }
-        }
-
-        Comparator<String> cmp = Comparators.comparingAlphaDecimal(Comparator.comparing(CharSequence::toString, String::compareToIgnoreCase));
-        fileNames.sort(cmp);
-        return fileNames;
-    }
 
     private FFMediaLoader createMediaLoader(List<String> fileNames) {
         return new FFMediaLoader(fileNames, ConverterApplication.getContext().next());
     }
 
-    public void selectFilesDialog() {
-        Window window = ConverterApplication.getEnv().getWindow();
-        final FileChooser fileChooser = new FileChooser();
-        String sourceFolder = AppProperties.getProperty("source.folder");
-        fileChooser.setInitialDirectory(Utils.getInitialDirecotory(sourceFolder));
-        StringJoiner filetypes = new StringJoiner("/");
-
-        Arrays.stream(FILE_EXTENSIONS).map(String::toUpperCase).forEach(filetypes::add);
-
-        fileChooser.setTitle("Select " + filetypes.toString() + " files for conversion");
-
-        fileChooser.getExtensionFilters().add(new FileChooser.ExtensionFilter("Audio", Arrays.asList(toSuffixes("*.", FILE_EXTENSIONS))));
-
-
-        List<File> files = fileChooser.showOpenMultipleDialog(window);
-        if (files != null) {
-            processFiles(files);
-            File firstFile = files.get(0);
-            File parentFile = firstFile.getParentFile();
-            AppProperties.setProperty("source.folder", parentFile.getAbsolutePath());
+    public void selectFiles() {
+        List<String> fileNames = DialogHelper.selectFilesDialog();
+        if (fileNames != null) {
+            processFiles(fileNames);
             if (!chaptersMode.get()) {
                 if (!filesChapters.getTabs().contains(filesTab)) {
                     filesChapters.getTabs().add(filesTab);
@@ -385,7 +320,7 @@ public class FilesController {
         ConversionContext context = ConverterApplication.getContext();
         if (context.next().getBook() == null && fileList.getItems().isEmpty()) return;
 
-        String outputDestination = selectOutputFile(ConverterApplication.getContext().next().getBookInfo());
+        String outputDestination = DialogHelper.selectOutputFile(ConverterApplication.getContext().next().getBookInfo());
 
         if (outputDestination == null) {
             return;
@@ -420,25 +355,6 @@ public class FilesController {
         fileList.getItems().clear();
         chaptersMode.set(false);
     }
-
-    private String selectOutputFile(AudioBookInfo audioBookInfo) {
-        JfxEnv env = ConverterApplication.getEnv();
-
-        final FileChooser fileChooser = new FileChooser();
-        String outputFolder = AppProperties.getProperty("output.folder");
-        fileChooser.setInitialDirectory(Utils.getInitialDirecotory(outputFolder));
-        fileChooser.setInitialFileName(Utils.getOuputFilenameSuggestion(audioBookInfo));
-        fileChooser.setTitle("Save AudioBook");
-        fileChooser.getExtensionFilters().addAll(
-                new FileChooser.ExtensionFilter(ConverterApplication.getContext().next().getOutputParameters().getFormat().toString(), "*." + ConverterApplication.getContext().next().getOutputParameters().getFormat().toString())
-        );
-        File file = fileChooser.showSaveDialog(env.getWindow());
-        if (file == null) return null;
-        File parentFolder = file.getParentFile();
-        AppProperties.setProperty("output.folder", parentFolder.getAbsolutePath());
-        return file.getPath();
-    }
-
 
     public void importChapters(ActionEvent actionEvent) {
         if (fileList.getItems().isEmpty()) {

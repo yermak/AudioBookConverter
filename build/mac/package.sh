a18585ed937f08e31af80318fdb761a78360fb47
Yermak <yermak@gmail.com>
2021-12-28 16:55:01 +0000

    packaging jdk for xodus

--- build/mac/package.sh
+++ build/mac/package.sh
@@ -8,7 +8,7 @@ mkdir target/release
 
 rm -rf target/fx-jre
 $JAVA_HOME/bin/jlink --module-path $JAVA_HOME/jmods:$JAVAFX_JMODS \
---add-modules java.base,java.sql,javafx.controls,javafx.fxml,javafx.media,javafx.base,javafx.swing,javafx.graphics --output target/fx-jre
+--add-modules java.base,java.sql,java.management,javafx.controls,javafx.fxml,javafx.media,javafx.base,javafx.swing,javafx.graphics --output target/fx-jre
 
 rm -rf target/image
 $JAVA_HOME/bin/jpackage --app-version $APP_VERSION  --icon AudioBookConverter.icns \

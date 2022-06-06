8930ccd4e3909df6b350e34994974b45f19fae81
Yermak <yermak@gmail.com>
2022-06-06 08:43:24 +0100

    Fixed track tag which could contain total number of tracks as well.

--- src/main/java/uk/yermak/audiobookconverter/AudioBookInfo.java
+++ src/main/java/uk/yermak/audiobookconverter/AudioBookInfo.java
@@ -18,12 +18,21 @@ public record AudioBookInfo(SmartStringProperty title, SmartStringProperty write
                             SmartStringProperty genre, SmartStringProperty year,
                             SmartIntegerProperty bookNumber, SmartIntegerProperty totalTracks,
                             SmartStringProperty comment,
-                            ObservableList<Track>tracks) {
+                            ObservableList<Track> tracks) {
 
 
     public static AudioBookInfo instance(Map<String, String> tags) {
-        String trackNumber = firstNonBlank(trimToEmpty(tags.get("track number")), trimToEmpty(tags.get("track")));
-        String trackCount = tags.get("track count");
+        int trackNumber = tags.get("track number") == null ? 0 : Integer.parseInt(tags.get("track number"));
+        int trackCount = tags.get("track count") == null ? 0 : Integer.parseInt(tags.get("track count"));
+
+        String track = tags.get("track");
+        if (StringUtils.isNotEmpty(track)) {
+            String[] split = StringUtils.split(track, "/");
+            trackNumber = Integer.parseInt(split[0]);
+            if (split.length > 1) {
+                trackCount = Integer.parseInt(split[1]);
+            }
+        }
 
         return new AudioBookInfo(
                 new SmartStringProperty(trimToEmpty(tags.get("title"))),
@@ -31,9 +40,9 @@ public record AudioBookInfo(SmartStringProperty title, SmartStringProperty write
                 new SmartStringProperty(firstNonBlank(trimToEmpty(tags.get("narratedby")), trimToEmpty(tags.get("composer")))),
                 new SmartStringProperty(trimToEmpty(tags.get("album"))),
                 new SmartStringProperty(trimToEmpty(tags.get("genre"))),
-                new SmartStringProperty(firstNonBlank(trimToEmpty(tags.get("year")), StringUtils.substring(trimToEmpty(tags.get("date")),0, 4))),
-                new SmartIntegerProperty(trackNumber == null ? 0 : Integer.parseInt(trackNumber)),
-                new SmartIntegerProperty(trackCount == null ? 0 : Integer.parseInt(trackCount)),
+                new SmartStringProperty(firstNonBlank(trimToEmpty(tags.get("year")), StringUtils.substring(trimToEmpty(tags.get("date")), 0, 4))),
+                new SmartIntegerProperty(trackNumber),
+                new SmartIntegerProperty(trackCount),
                 new SmartStringProperty(firstNonBlank(trimToEmpty(tags.get("comment")), trimToEmpty(tags.get("comment-0")))),
                 FXCollections.observableArrayList()
         );
